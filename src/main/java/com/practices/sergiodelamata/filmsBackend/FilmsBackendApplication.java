8407d16b45ef264b0eaf62a579dbc31958b00ced
SergiodelaMata <sergio.matam@edu.uah.es>
2022-01-21 12:18:35 +0100

    :sparkles: Added structure from Spring Boot Framework.

---
+++ src/main/java/com/practices/sergiodelamata/filmsBackend/FilmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.practices.sergiodelamata.filmsBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practices/sergiodelamata/filmsBackend/FilmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practices.sergiodelamata.filmsBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
