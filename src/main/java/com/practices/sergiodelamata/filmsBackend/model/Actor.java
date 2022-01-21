36e61c9016ac3e9b63580ad93f48dfb3f203c3d5
SergiodelaMata <sergio.matam@edu.uah.es>
2022-01-22 00:15:50 +0100

    :sparkles: Created the entities from the model of the application.

---
+++ src/main/java/com/practices/sergiodelamata/filmsBackend/model/Actor.java
@@ -0,0 +1,106 @@
+package com.practices.sergiodelamata.filmsBackend.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "actor")
+public class Actor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idActor", nullable = false)
+    private Integer idActor;
+
+    @Lob
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "birthDate")
+    private LocalDate birthDate;
+
+    @Lob
+    @Column(name = "countryBirth", nullable = false)
+    private String countryBirth;
+
+    @ManyToMany
+    /*@JoinTable(name = "participate",
+            joinColumns = @JoinColumn(name = "idActor"),
+            inverseJoinColumns = @JoinColumn(name = "idFilm"))
+    Esto no funciona, se ha cambiado a lo siguiente:
+    */
+    @JoinTable(name = "participate", joinColumns = {
+            @JoinColumn(name="idActor", referencedColumnName = "idActor")},
+            inverseJoinColumns = {@JoinColumn(name="idFilm", referencedColumnName = "idFilm")})
+    @JsonIgnoreProperties("actor")
+    private List<Film> films = new ArrayList<>();
+
+    public List<Film> getFilms() {
+        return films;
+    }
+
+    public void setFilms(List<Film> films) {
+        this.films = films;
+    }
+
+    public String getCountryBirth() {
+        return countryBirth;
+    }
+
+    public void setCountryBirth(String countryBirth) {
+        this.countryBirth = countryBirth;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getIdActor() {
+        return idActor;
+    }
+
+    public void setIdActor(Integer idActor) {
+        this.idActor = idActor;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(this == o) return true;
+        if(!(o instanceof Actor)) return false;
+        Actor actor = (Actor) o;
+        return Objects.equals(idActor, actor.idActor);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(idActor);
+    }
+
+    public void addFilm(Film film){
+        if(film != null){
+            getFilms().add(film);
+        }
+    }
+
+    public void removeFilm(Film film){
+        if(film != null){
+            getFilms().remove(film);
+        }
+    }
+}
---
+++ src/main/java/com/practices/sergiodelamata/filmsBackend/model/Film.java
@@ -0,0 +1,156 @@
+package com.practices.sergiodelamata.filmsBackend.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "film")
+public class Film {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idFilm", nullable = false)
+    private Integer idFilm;
+
+    @Lob
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "year", nullable = false)
+    private Integer year;
+
+    @Column(name = "duration", nullable = false)
+    private Integer duration;
+
+    @Lob
+    @Column(name = "country", nullable = false)
+    private String country;
+
+    @Lob
+    @Column(name = "direction", nullable = false)
+    private String direction;
+
+    @Lob
+    @Column(name = "genres", nullable = false)
+    private String genres;
+
+    @Lob
+    @Column(name = "synopsis", nullable = false)
+    private String synopsis;
+
+    @Column(name = "image", nullable = false)
+    private byte[] image;
+
+    @ManyToMany(mappedBy = "films")
+    @JsonIgnoreProperties("film")
+    private List<Actor> actors = new ArrayList<>();
+
+    public List<Actor> getActors() {
+        return actors;
+    }
+
+    public void setActors(List<Actor> actors) {
+        this.actors = actors;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public String getGenres() {
+        return genres;
+    }
+
+    public void setGenres(String genres) {
+        this.genres = genres;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getIdFilm() {
+        return idFilm;
+    }
+
+    public void setIdFilm(Integer idFilm) {
+        this.idFilm = idFilm;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(this == o) return true;
+        if(!(o instanceof Film)) return false;
+        Film film = (Film) o;
+        return Objects.equals(idFilm, film.idFilm);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(idFilm);
+    }
+
+    public void addFilm(Actor actor){
+        if(actor != null){
+            getActors().add(actor);
+        }
+    }
+
+    public void removeFilm(Actor actor){
+        if(actor != null){
+            getActors().remove(actor);
+        }
+    }
+
+}
