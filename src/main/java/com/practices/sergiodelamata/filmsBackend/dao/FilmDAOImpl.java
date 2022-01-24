6324ed07316131e2c88ec5bc41c7b8e3b98ec3a4
SergiodelaMata <sergio.matam@edu.uah.es>
2022-01-24 17:43:24 +0100

    :fire: Clean up of code.

--- src/main/java/com/practices/sergiodelamata/filmsBackend/dao/FilmDAOImpl.java
+++ src/main/java/com/practices/sergiodelamata/filmsBackend/dao/FilmDAOImpl.java
@@ -5,8 +5,6 @@ import com.practices.sergiodelamata.filmsBackend.model.Film;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
